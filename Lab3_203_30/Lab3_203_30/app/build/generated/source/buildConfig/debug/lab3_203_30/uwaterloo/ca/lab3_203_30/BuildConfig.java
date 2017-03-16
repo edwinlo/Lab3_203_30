8cac3be875646092ea8f6eccefe9aa6c0f689e0d
Edwin <edwinytlo@gmail.com>
2017-03-16 00:44:22 -0400

    testing' '

---
+++ Lab3_203_30/Lab3_203_30/app/build/generated/source/buildConfig/debug/lab3_203_30/uwaterloo/ca/lab3_203_30/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package lab3_203_30.uwaterloo.ca.lab3_203_30;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "lab3_203_30.uwaterloo.ca.lab3_203_30";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
